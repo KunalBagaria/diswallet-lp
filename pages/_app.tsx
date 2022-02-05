15ebf6a6a9cf2f6f6314bb3e397ff55ce2b9dd38
Kunal Bagaria <kb24x7@gmail.com>
2022-02-05 23:46:43 +0530

    init: all files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.scss';
+import React from 'react';
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const LP = function LP({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+};
 
-export default MyApp
+export default LP;
