739f6871f3cacd99ac29cb0b0ee302a871552240
Kunal Bagaria <kb24x7@gmail.com>
2022-02-14 00:24:36 +0530

    test: og image

--- layouts/Head.tsx
+++ layouts/Head.tsx
@@ -7,6 +7,9 @@ export const DefaultHead = function DefaultHead() {
       <title>DisWallet</title>
       <meta name="description" content="The Solana Wallet that works inside Discord" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta property="og:title" content="DisWallet" />
+      <meta property="og:description" content="The Solana Wallet that works inside Discord" />
+      <meta property="og:image" content="https://i0.wp.com/c.tenor.com/K9XG-Ir3F0oAAAAd/bunny-rabbit.gif" />
       <link rel="icon" href="/favicon.png" />
     </Head>
   );
