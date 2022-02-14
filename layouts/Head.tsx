7400d828e0d54d082f8f051ec411d441a1236850
Kunal Bagaria <kb24x7@gmail.com>
2022-02-14 17:23:25 +0530

    update: meta image

--- layouts/Head.tsx
+++ layouts/Head.tsx
@@ -10,16 +10,16 @@ export const DefaultHead = function DefaultHead() {
       <link rel="icon" href="/favicon.png" />
 
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://diswallet.vercel.app/" />
+      <meta property="og:url" content="https://diswallet.app/" />
       <meta property="og:title" content="DisWallet" />
       <meta property="og:description" content="The Solana Wallet that works inside Discord" />
-      <meta property="og:image" content="https://i0.wp.com/c.tenor.com/K9XG-Ir3F0oAAAAd/bunny-rabbit.gif" />
+      <meta property="og:image" content="https://i0.wp.com/ipfs.io/ipfs/bafybeib3h6577jm42o7qklmcf2qawkjqlo4cjybir5yj7gev56rpgo2qyy" />
 
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://diswallet.vercel.app/" />
+      <meta property="twitter:url" content="https://diswallet.app/" />
       <meta property="twitter:title" content="DisWallet" />
       <meta property="twitter:description" content="The Solana Wallet that works inside Discord" />
-      <meta property="twitter:image" content="https://i0.wp.com/c.tenor.com/K9XG-Ir3F0oAAAAd/bunny-rabbit.gif" />
+      <meta property="twitter:image" content="https://i0.wp.com/ipfs.io/ipfs/bafybeib3h6577jm42o7qklmcf2qawkjqlo4cjybir5yj7gev56rpgo2qyy" />
     </Head>
   );
 };
